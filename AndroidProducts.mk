e001bc1aebc9ba22dc5faa02bb30376cbb04fe13
HoleHolo <hola0_o@qq.com>
2023-08-05 21:39:15 +0800

    Git inital

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_cornell.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_cornell-user \
+    twrp_cornell-userdebug \
+    twrp_cornell-eng
